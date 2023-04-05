ab7a2e8638f72cb758e53f7c8f70c18a8c1b7243
Un Focused <unfocused@Uns-iMac-3.local>
2023-04-04 17:52:06 -0700

    working implementation of get all documents

--- src/main.rs
+++ src/main.rs
@@ -1,71 +1,93 @@
-use mongodb::{Client, Collection, options::{ClientOptions, ResolverConfig}};
+use mongodb::{Client, Collection};
+use rocket::futures::StreamExt;
+use rocket::{serde::json::Json};
 use std::env;
 use std::process::exit;
-use mongodb::bson::Document;
-use rocket::futures::StreamExt;
+use mongodb::bson::de::Error;
+use mongodb::bson::{doc, Document};
+use rocket::http::Status;
+use rocket::{State};
+
 // use mongodb::bson::Document;
 // use rocket::futures::StreamExt;
+// extern crate dotenv;
+
+use dotenv::dotenv;
 
 #[macro_use]
 extern crate rocket;
 
 #[get("/")]
-fn index() -> &'static str {
-    return "Hello";
+async fn index(
+    repo: &State<MongoRepo>
+) -> Result<Json<Vec<Document>>, Status> {
+    let transactions = repo.get_transactions().await.unwrap();
+    let res = Json(transactions);
+
+    Ok(res)
+}
+
+struct MongoRepo {
+    collection: Collection::<Document>
+}
+
+impl MongoRepo {
+    pub async fn init() -> Self {
+        dotenv().ok();
+        // Load the MongoDB connection string from an environment variable:
+        let client_uri = match env::var("MONGODB_URI") {
+            Ok(v) => v.to_string(),
+            Err(_) => format!("Error loading evn variable"),
+        };
+
+        let client = Client::with_uri_str(client_uri).await;
+
+        if let Err(_err) = client {
+            exit(1);
+        }
+
+        let client = client.unwrap();
+
+        let transactions_collection = client.database("vybe").collection::<Document>("transactions");
+
+        MongoRepo {
+            collection: transactions_collection
+        }
+    }
+
+    pub async fn get_transactions(&self) -> Result<Vec<Document>, Error> {
+        let cursor = self.collection.find(None, None).await.ok();
+
+        let mut cursor = cursor.unwrap();
+
+        let mut docs: Vec<Document> = Vec::new();
+
+        // REFERENCE: ChatGPT
+        while let Some(document) = cursor.next().await {
+            match document {
+                Ok(doc) => {
+                    docs.push(doc)
+                }
+                Err(err) => {
+                    println!("Failed to retrieve document: {:?}", err)
+                }
+            }
+        }
+
+        Ok(docs)
+    }
 }
 
 #[shuttle_runtime::main]
 async fn rocket() -> shuttle_rocket::ShuttleRocket {
-    // Load the MongoDB connection string from an environment variable:
-    let client_uri = env::var("MONGODB_URI").expect("You must set the MONGODB_URI environment var!");
-
-    // A Client is needed to connect to MongoDB:
-    // An extra line of code to work around a DNS issue on Windows:
-    let options = ClientOptions::parse_with_resolver_config(&client_uri, ResolverConfig::cloudflare())
-        .await;
-    if let Err(_err) = options {
-        exit(1);
-    }
-
-    let options = options.unwrap();
-    let client = Client::with_options(options);
-
-    if let Err(_err) = client {
-        exit(1);
-    }
-
-    let client = client.unwrap();
-
-    let transactions_collection: Collection<Document> = client.database("vybe").collection("transactions");
-    // let cursors: Cursor<Document> = transactions_collection.find(None, None).ok().expect("Error getting trasnactions");
-    let cursor = transactions_collection.find(None, None).await;
-    if let Err(_err) = cursor {
-        exit(1);
-    }
-
-    let mut cursor = cursor.unwrap();
-
-    // REFERENCE: ChatGPT
-    while let Some(document) = cursor.next().await {
-        match document {
-            Ok(doc) => {
-                println!("Document: {:?}", doc);
-            }
-            Err(err) => {
-                println!("Failed to retrieve document: {:?}", err)
-            }
-        }
-    }
-
-    // let docs = cursors.collect();
-    // let transactions = cursors.map(|doc| doc.unwrap()).collect();
+    let mongo_repo = MongoRepo::init().await;
 
     // Print the databases in our MongoDB cluster:
     println!("Databases:");
     // for name in client.list_database_names(None, None).await? {
     //     println!("- {}", name);
     // }
-    let rocket = rocket::build().mount("/", routes![index]);
+    let rocket = rocket::build().manage(mongo_repo).mount("/", routes![index]);
 
     Ok(rocket.into())
 }
